edd15118c1365a2e9e81e640eafaa322e303d466
krydge <k.rydge.stubbs@gmail.com>
2019-11-06 11:23:50 -0700

    added student model and db context

---
+++ Student/Data/OurDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Data
+{
+    public class OurDbContext:IdentityDbContext
+    {
+        public OurDbContext(DbContextOptions<OurDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
