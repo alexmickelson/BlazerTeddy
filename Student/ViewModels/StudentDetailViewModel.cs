d96e29ec92ba7ce43d0ea48bcfe2abeb7853346a
krydge <k.rydge.stubbs@gmail.com>
2019-11-11 11:19:34 -0700

    added a view model

---
+++ Student/ViewModels/StudentDetailViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Student.Models;
+using Student.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.ViewModels
+{
+    public class StudentDetailViewModel
+    {
+        public readonly StudentRepository studentRepository;
+        public StudentInfo student;
+        public string newComment;
+        public string errorAlert;
+
+        public StudentDetailViewModel(StudentRepository studentRepository, int studentID)
+        {
+            this.studentRepository = studentRepository;
+            student = this.studentRepository.GetStudent(studentID);
+
+        }
+        public void addNote()
+        {
+            if (String.IsNullOrEmpty(newComment))
+            {
+                errorAlert = "Note cannot be Empty";
+            }
+            else
+            {
+                student.Notes.Add(new Note()
+                {
+                    Content = newComment
+                });
+                errorAlert = "";
+                newComment = "";
+            }
+        }
+
+    }
+}
