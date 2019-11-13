8413eab7b8dce8b222a194a7f5f548d108afe8a8
Alex Mickelson <alexmickelson96@gmail.com>
2019-11-13 13:04:47 -0700

    can test context with in memory database now

---
+++ Test/ServiceTests/StudentRepositoryTests.cs
@@ -0,0 +1,49 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using Student.Data;
+using Student.Models;
+using Student.Services;
+
+namespace Test.ServiceTests
+{
+    public class StudentRepostoryTests
+    {
+        private DbContextOptions<OurDbContext> dbOptions;
+        private StudentRepository studentRepostiory;
+        private OurDbContext context;
+
+        [SetUp]
+        public void Setup()
+        {
+
+            var dbOptions = new DbContextOptionsBuilder<OurDbContext>()
+                .UseInMemoryDatabase(databaseName: "StudentRepository")
+                .Options;
+            context = new OurDbContext(dbOptions);
+            studentRepostiory = new StudentRepository(context);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+
+        [Test]
+        public void EditingReferenceOfStudentListEditsRepostoryList()
+        {
+            var students = studentRepostiory.GetStudents();
+            students.Append(new StudentInfo(){
+                StudentInfoId = 3,
+                Name = "Sam"
+            });
+
+            students.Should().BeSameAs(studentRepostiory.GetStudents());
+        }
+    }
+}
