704ef4def18d0b01a250d30422967d39ca36770b
krydge <k.rydge.stubbs@gmail.com>
2019-11-14 14:19:27 -0700

    Added ClassRoomModel

---
+++ Student/Models/ClassRoomModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Models
+{
+    public class ClassRoomModel
+    {
+
+        public int Id{ get; set; }
+        public List<StudentInfo> Students { get; set; }
+
+        //public List<TeacherInfo> Teachers { get; set; }
+
+        //public SeatingChart seatingChart { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
