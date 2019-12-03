d546ab4d13020a1f892865480b44edeec76d7c09
Alex Mickelson <alexmickelson96@gmail.com>
2019-12-02 19:21:14 -0700

    Prepared app for heroku deployment

--- postgres/init.ps1
+++ postgres/init.ps1
@@ -1,3 +1,3 @@
 
 docker rm -f teddy_postgres;
-docker run --name teddy_postgres -p 5432:5432 -v ${pwd}\startup:/docker-entrypoint-initdb.d/ -e POSTGRES_USER=teddy -e POSTGRES_PASSWORD=teddy postgres
+docker run --name teddy_postgres -p 5432:5432 -v ${pwd}\startup:/docker-entrypoint-initdb.d/ -e POSTGRES_DB=teddy  -e POSTGRES_USER=teddy -e POSTGRES_PASSWORD=teddy postgres
