85dc8cc23be40e984a50217d72a3dca0ce41f9d8
Alex Mickelson <alexmickelson96@gmail.com>
2019-12-04 11:44:53 -0700

    found othe blazer

--- blazorTeddy/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
+++ blazorTeddy/Areas/Identity/RevalidatingIdentityAuthenticationStateProvider.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace TeddyBlazer.Areas.Identity
+namespace TeddyBlazor.Areas.Identity
 {
     public class RevalidatingIdentityAuthenticationStateProvider<TUser>
         : RevalidatingServerAuthenticationStateProvider where TUser : class
--- blazorTeddy/Startup.cs
+++ blazorTeddy/Startup.cs
@@ -7,7 +7,7 @@ using Dapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
